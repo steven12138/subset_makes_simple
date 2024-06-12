69265315bb525137257a6fc941e42a65dcd7a915
steven12138 <18611209860@163.com>
2024-06-12 21:46:24 +0800

    d

--- src/dp.cpp
+++ src/dp.cpp
@@ -20,6 +20,35 @@ void Backpack() {
     }
 }
 
+
+bool choice[MAXN];
+
+void TraceBack(int x, int rest) {
+    if (x == 1) {
+        if (rest == seq[1]) {
+            choice[1] = 1, rest = 0;
+        }
+        if (!rest) {
+            std::cout << "sum: " << m << std::endl;
+            for (int i = 1; i <= n; i++)
+                if (choice[i])
+                    std::cout << seq[i] << " ";
+            std::cout << std::endl;
+            for (int i = 1; i <= n; i++)
+                std::cout << choice[i];
+            std::cout << std::endl;
+        }
+        choice[1] = 0;
+        return;
+    }
+    if (f[x][rest - seq[x]] == rest - seq[x]) {
+        choice[x] = 1;
+        TraceBack(x - 1, rest - seq[x]);
+        choice[x] = 0;
+    }
+    if (f[x][rest] == rest) TraceBack(x - 1, rest);
+}
+
 int main(int argc, char *argv[]) {
     freopen("data/in.in", "r", stdin);
 
@@ -30,5 +59,6 @@ int main(int argc, char *argv[]) {
     }
 
     Backpack();
+    TraceBack(n, m);
     return 0;
 }
